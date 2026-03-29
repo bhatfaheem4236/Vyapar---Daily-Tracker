c7cafd5e7c175ca0b618a619f91c54baf1dd42a9
bhatfaheem4236 <bhatfaheem4236@gmail.com>
2026-03-29 12:53:05 +0530

    remove unnecessary files

--- qr_code.py
+++
@@ -1,3 +0,0 @@
-import qr_code as qr
-img = qr("https://studio.youtube.com/channel/UCD8SGFDHcI8a_toBqE6otUw/videos/upload?filter=%5B%5D&sort=%7B%22columnType%22%3A%22date%22%2C%22sortOrder%22%3A%22DESCENDING%22%7D")
-img.save("faheem_wadipora.png")
